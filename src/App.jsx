652f8c79d021f73a71a41d05de76442e5b4d1450
Roberto Aranda <ese.rober@gmail.com>
2022-10-22 11:58:25 +0200

    Add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
+import reflameLogo from './icon.svg';
 import './App.css';
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <div>
+        <a href="https://reflame.app" target="_blank">
+          <img src={reflameLogo} className="logo" alt="Reflame logo" />
+        </a>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Reflame + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
